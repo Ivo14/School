065f67cfd456dac7b6f5c7aa88c3dda3105aa183
Ivo14 <70132844+Ivo14@users.noreply.github.com>
2021-03-25 21:41:28 +0200

    epic

---
+++ programirane/If else/3ta_zad_2upr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+int e;
+cout<<"Vuvedi chisloto\n";
+cin>>e;
+{
+    if (e%2==0)
+        cout<<"even number";
+        else
+        cout<<"odd number";
+}
+ return 0;
+}
+//Въвежда се естествено число. Да се изведе подходящо съобщение „even number“ (четно число) или „odd number“(нечетно).
